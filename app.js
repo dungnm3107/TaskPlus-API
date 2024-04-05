c35183a7b559178b338f2f64f8b2bbdaafb04616
NotnullServer <manhdung1602003@gmail.com>
2024-04-05 11:35:16 +0700

    fix_cors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // app.use(cors());
 app.use(cors({
-    origin: 'https://task-plus-web.vercel.app/login',
+    origin: 'https://task-plus-web.vercel.app/',
     optionsSuccessStatus: 200 
   }));
 
